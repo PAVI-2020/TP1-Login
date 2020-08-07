6a1e6db79f604c991b172980dc1a1d9bdffb9e13
SimplyBLGDev <61010688+SimplyBLGDev@users.noreply.github.com>
2020-08-07 15:51:05 -0300

    Conflicto

--- TP1-Login/TP1-Login/frmMain.cs
+++ TP1-Login/TP1-Login/frmMain.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace TP1_Login {
 	public partial class frmMain : Form {
 
-		// Comentario para generar un conflicto
+		// Diferente comentario para generar un conflicto
 		private string correctUsername = "admin";
 		private string correctCarrera = "Sistemas";
 		private string correctPassword = "1234";
